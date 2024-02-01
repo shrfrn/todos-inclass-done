0689f0debf842f2600cc4f4d1b02acb59655bf7d
shrfrn <shrfrn@gmail.com>
2024-02-01 17:50:12 +0200

    Add dynamic rendering

--- js/todos.js
+++ js/todos.js
@@ -1,6 +1,23 @@
 'use strict'
 
-console.log('Hi')
+var gTodos = [
+    { id: 't101', txt: 'Do this', isDone: false },
+    { id: 't102', txt: 'Do that', isDone: true },
+    { id: 't103', txt: 'Try this', isDone: false },
+]
+
+function onInit() {
+    renderTodos()
+}
+
+function renderTodos() {
+    const elTodos = document.querySelector('.todo-list')
+    const strHtmls = gTodos.map(todo => `<li>
+            <span class="${todo.isDone ? 'done' : ''}">${todo.txt}</span>
+            <button onclick="onRemoveTodo('${todo.id}')">x</button>
+        </li>`)
+    elTodos.innerHTML = strHtmls.join('')
+}
 
 function onRemoveTodo(todoId) {
     console.log('todoId: ', todoId)

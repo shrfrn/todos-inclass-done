de87d6d0da4311b4648606f8652cc6c6d150d671
shrfrn <shrfrn@gmail.com>
2024-02-01 20:15:19 +0200

    Add todo factory functions and use makeId()

--- js/services/todo.service.js
+++ js/services/todo.service.js
@@ -1,50 +1,68 @@
 'use strict'
 
-var gTodos = [
-    { id: 't101', txt: 'Do this', isDone: false },
-    { id: 't102', txt: 'Do that', isDone: true },
-    { id: 't103', txt: 'Try this', isDone: false },
-]
+var gTodos
+_createTodos()
 
-function getTodos(filterBy) { // List
-    if(!filterBy) return gTodos
+function getTodos(filterBy) {
+	// List
+	if (!filterBy) return gTodos
 
-    const isDone = (filterBy === 'Done')
-    return gTodos.filter(todo => todo.isDone === isDone)
+	const isDone = filterBy === 'Done'
+	return gTodos.filter(todo => todo.isDone === isDone)
 
-    // return gTodos.filter(todo => 
-    //     todo.isDone === (filterBy === 'Done') ? true : false)
-
+	// return gTodos.filter(todo =>
+	//     todo.isDone === (filterBy === 'Done') ? true : false)
 }
 
-function removeTodo(todoId) {  // Delete
-    const idx = gTodos.findIndex(todo => todo.id === todoId)
-    gTodos.splice(idx, 1)
+function removeTodo(todoId) {
+	// Delete
+	const idx = gTodos.findIndex(todo => todo.id === todoId)
+	gTodos.splice(idx, 1)
 }
 
-function toggleTodo(todoId) { // Update
-    const todo = gTodos.find(todo => todo.id === todoId)
-    todo.isDone = !todo.isDone
+function toggleTodo(todoId) {
+	// Update
+	const todo = gTodos.find(todo => todo.id === todoId)
+	todo.isDone = !todo.isDone
+
+	return todo
 }
 
-function readTodo(todoId) { // Read
-    const todo = gTodos.find(todo => todo.id === todoId)
-    return todo
+function readTodo(todoId) {
+	// Read
+	const todo = gTodos.find(todo => todo.id === todoId)
+	return todo
 }
 
-function addTodo(txt) { // Create
-    const todo = {
-        id: 't' + Date.now() % 1000,
-        txt,
-        isDone: false,
-    }
-    gTodos.unshift(todo)
+function addTodo(txt) {
+	// Create
+	const todo = _createTodo(txt)
+	gTodos.unshift(todo)
+	return todo
 }
 
 function getTotalTodoCount() {
-    return gTodos.length
+	return gTodos.length
 }
 
 function getActiveTodoCount() {
-    return gTodos.filter(todo => todo.isDone === false).length
+	return gTodos.filter(todo => todo.isDone === false).length
+}
+
+// Private functions
+
+function _createTodos() {
+    gTodos = [
+        _createTodo('Do this'), 
+        _createTodo('Do that'), 
+        _createTodo('Try this')
+    ]
+}
+
+function _createTodo(txt) {
+	return {
+		id: makeId(),
+		txt,
+		isDone: false,
+	}
 }
